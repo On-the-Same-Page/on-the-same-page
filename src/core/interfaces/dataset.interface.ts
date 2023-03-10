c150accaa4ef44b96dab42ee9675a96cfa918209
tiagombp <tiagombp@gmail.com>
2023-03-10 13:53:16 +0100

    replaces "novels" button for "other", changes order of the buttons

--- src/core/interfaces/dataset.interface.ts
+++ src/core/interfaces/dataset.interface.ts
@@ -2,14 +2,14 @@
  * The list of filterable genres.
  */
 export enum Genre {
-    SCIENCE_FICTION = "Science Fiction",
-    HISTORICAL_FICTION = "Historical Fiction",
-    NOVELS = "Novels",
-    THRILLER = "Thriller",
-    MYSTERY = "Mystery",
-    ROMANCE = "Romance",
     FANTASY = "Fantasy",
+    ROMANCE = "Romance",
+    HISTORICAL_FICTION = "Historical Fiction",
+    MYSTERY = "Mystery",
+    SCIENCE_FICTION = "Science Fiction",
+    THRILLER = "Thriller",
     NONFICTION = "Nonfiction",
+    OTHER = "Other",
 }
 
 export type RawDataSet = any; // TODO: TYPE!

7b2dbb73427ecafbc45a52e99954871c894f6bd1
Markus Wagner <prttmprpht@gmail.com>
2023-03-11 22:15:37 +0100

    Added intro animation

--- src/root/root.component.ts
+++ src/root/root.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from "@angular/core";
+import {Component, ElementRef, OnInit, ViewChild} from "@angular/core";
 
 @Component({
     selector: "otsp-root",
@@ -6,11 +6,23 @@ import {Component, OnInit} from "@angular/core";
     styleUrls: ["./root.component.scss"]
 })
 export class RootComponent implements OnInit {
+    @ViewChild("vid", {static: true}) vid!: ElementRef<HTMLVideoElement>;
+
+    displayStartButton = true;
+    displayAnimation = true;
     rawDataSet: any | null = null;
 
     public async ngOnInit(): Promise<void> {
+        const res = await fetch("assets/animation.mp4");
+        const blob = await res.blob();
+        this.vid.nativeElement.src = URL.createObjectURL(blob);
+
         const response = await fetch("assets/data.json");
         const jsonData = await response.json();
         this.rawDataSet = jsonData;
+    }
+
+    hideAnimation() {
+        this.displayAnimation = false;
     }
 }

3985f1711b85fec9e095ff8a8389cbccafa7dd2c
tiagombp <tiagombp@gmail.com>
2023-03-19 07:31:25 +0100

    minor adjustments, adds link to fav book on authors page

--- src/core/components/authors-modal/authors-modal.component.ts
+++ src/core/components/authors-modal/authors-modal.component.ts
@@ -13,26 +13,31 @@ export class AuthorsModalComponent {
         name: "Maria Zelenika",
         email: "zelenika@kth.se",
         work: ["Art", "UI / UX design", "User test"],
-        book: "Storm and Silence"
+        book: "Storm and Silence",
+        url: "https://www.goodreads.com/book/show/24035804-storm-and-silence"
     }, {
         name: "Markus Wagner",
         email: "wagn@kth.se",
         work: ["Data handling & processing", "Frontend development", "UI / UX design"],
-        book: "What if?"
+        book: "What if?",
+        url: "https://www.goodreads.com/book/show/21413662-what-if-serious-scientific-answers-to-absurd-hypothetical-questions"
     }, {
         name: "Tiago M B Pereira",
         email: "tiagombp@kth.se",
         work: ["Data handling & processing", "Frontend development", "User test"],
-        book: "Jonathan Strange & Mr. Norrell"
+        book: "Jonathan Strange & Mr. Norrell",
+        url: "https://www.goodreads.com/book/show/14201.Jonathan_Strange_Mr_Norrell"
     }, {
         name: "Rurik Högberg",
         email: "rurikho@kth.se",
         work: ["Data handling & processing", "UI / UX design", "User test"],
-        book: "Confessions of an Heiress"
+        book: "Confessions of an Heiress",
+        url: "https://www.goodreads.com/book/show/87564.Confessions_of_an_Heiress"
     }, {
         name: "Wanqing Zhou",
         email: "wanqingz@kth.se",
         work: ["Data handling & processing", "Art", "UI / UX design"],
-        book: "The Romance of the Three Kingdoms"
+        book: "The Romance of the Three Kingdoms",
+        url: "https://www.goodreads.com/book/show/42634134-the-romance-of-the-three-kingdoms"
     }];
 }
